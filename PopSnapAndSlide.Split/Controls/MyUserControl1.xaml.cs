e0c01a626aa57e8918496ccdbada6136a8ec9d01
Michael L Perry <michael@qedcode.com>
2013-04-02 20:22:15 -0500

    Snapped

---
+++ PopSnapAndSlide.Split/Controls/MyUserControl1.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using PopSnapAndSlide.Split.Common;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace PopSnapAndSlide.Split.Controls
+{
+    public sealed partial class MyUserControl1 : UserControl
+    {
+        public MyUserControl1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void UserControl_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            var page = GetPage();
+            if (page != null)
+                page.StartLayoutUpdates(sender, e);
+        }
+
+        private void UserControl_Unloaded_1(object sender, RoutedEventArgs e)
+        {
+            var page = GetPage();
+            if (page != null)
+                page.StopLayoutUpdates(sender, e);
+        }
+
+        private LayoutAwarePage GetPage()
+        {
+            var ancestor = this.Parent as FrameworkElement;
+
+            while (ancestor != null)
+            {
+                var container = ancestor as LayoutAwarePage;
+                if (container != null)
+                    return container;
+
+                ancestor = ancestor.Parent as FrameworkElement;
+            }
+
+            return null;
+        }
+    }
+}
